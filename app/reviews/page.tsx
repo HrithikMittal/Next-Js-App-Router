865ecfb3a305b5589ab73012a84902e07a4706e9
Adhikansh Mittal <adhikanshmittal@gmail.com>
2023-07-09 08:41:07 +0530

    add link in next

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -2,6 +2,12 @@ const Reviews = () => {
   return (
     <>
       <h1>Reviews</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque
+        molestiae quibusdam adipisci ut exercitationem odit a iste enim
+        obcaecati sunt recusandae iure, laboriosam reprehenderit, blanditiis,
+        odio maxime doloremque nisi repellendus.
+      </p>
     </>
   );
 };
